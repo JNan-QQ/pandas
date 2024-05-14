4124449c94f2fc5cd90e427f9c8f362167227198
jiangnan <896333574@qq.com>
2024-05-14 18:20:57 +0800

    更新许可证信息

--- dataframe/file.go
+++ dataframe/file.go
@@ -1,6 +1,6 @@
 /**
   Copyright (c) [2024] [JiangNan]
-  [go-tools] is licensed under Mulan PSL v2.
+  [pandas] is licensed under Mulan PSL v2.
   You can use this software according to the terms and conditions of the Mulan PSL v2.
   You may obtain a copy of Mulan PSL v2 at:
            http://license.coscl.org.cn/MulanPSL2
